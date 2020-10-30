2a846343e3df06b163595c8f673c14b9fcbcc2b8
조규성 <pandora_h@naver.com>
2020-10-30 20:38:11 +0900

    UPDATE | coordi set recommendation logic

--- HeoSeongSoo/pages/CodiFormScreen.js
+++ HeoSeongSoo/pages/CodiFormScreen.js
@@ -1,16 +1,121 @@
-import React, { useState } from 'react';
-import { Alert, Modal, StyleSheet, Text, TouchableHighlight, View } from 'react-native';
-import { Camera } from 'expo-camera';
-
+import React from 'react';
+import { Alert, Modal, StyleSheet, Text, TouchableHighlight, View, Image } from 'react-native';
+import axios from 'axios'
+import * as ImagePicker from 'expo-image-picker';
 
 function CodiFormScreen({ navigation }) {
     const [uploadCategory, setUploadCategory] = React.useState();
     const [modalVisible, setModalVisible] = React.useState(false);
-    const [images, setimages] = React.useState([]);
-    
-    const onSelect = data => {
+    const [image, setImage] = React.useState(null);
 
+    React.useEffect(() => {
+      (async () => {
+        if (Platform.OS !== 'web') {
+          const { status } = await ImagePicker.requestCameraRollPermissionsAsync();
+          if (status !== 'granted') {
+            alert('Sorry, we need camera roll permissions to make this work!');
+          }
+        }
+      })();
+    }, []);
+
+    const pickImage = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+          mediaTypes: ImagePicker.MediaTypeOptions.All,
+          allowsEditing: true,
+          aspect: [4, 3],
+          quality: 1,
+        });
+    
+        console.log(result);
+    
+        if (!result.cancelled) {
+          setImage(result.uri);
+        }
+      };
+
+    const onSelect = data => {
+        // 카메라에서 데이터가 오면 처리해야함
     }
+
+    const recommendationRequest = () => {
+        // 서버로 이미지를 보내고 결과를 받아옵니다.
+        // axios.post()
+        //.then(res => {
+        const res = {
+            data: [
+                {
+                    id: 1,
+                    img: 'https://i0.codibook.net/files/thumb/big/197407283459/d6b9201b8871b2/639666474.jpg',
+                    content: '가을에 어울리는 코디lllllllllllllllllllllllㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣ',
+                    liked: false,
+                    likes: 10,
+                    user: '박인영',
+                    items: [
+                        {},
+                        {
+                            id: '1',
+                            category: 'top',
+                            name: '사인 로고 후디 그레이',
+                            price: '55,200',
+                            url: 'https://store.musinsa.com/app/product/detail/644026/0',
+                        },
+                        {
+                            id: '2',
+                            category: 'pants',
+                            name: 'Punk Town - MOD4 crop',
+                            price: '73,500',
+                            url: 'https://store.musinsa.com/app/product/detail/1037219/0'
+                        },
+                        {
+                            id: '3',
+                            category: 'outer',
+                            name: '네이비 하운드투스 울 블렌디드 레귤러핏 싱글 재킷 (320X11LY5R)',
+                            price: '119,900',
+                            url: 'https://store.musinsa.com/app/product/detail/1646140/0',
+                        },
+                        {
+                            id: '4',
+                            category: 'shoes',
+                            name: '바스토 베라티 (BASTOW VERRATI (GLASSY WHITE)) [BTVA61-GW09]',
+                            price: '179,000',
+                            url: 'https://store.musinsa.com/app/product/detail/1638849/0'
+                        }
+                    ]
+                },
+                {
+                    id: 2,
+                    img: 'https://t1.daumcdn.net/cfile/tistory/2422174D5244C0461F',
+                    content: '깔끔한 코디입니다.',
+                    liked: true,
+                    likes: 5,
+                    user: '박도희',
+                    items: [{}, {}, {}, {}, {}]
+                },
+                {
+                    id: 3,
+                    img: 'https://t1.daumcdn.net/cfile/tistory/210532355348EB1A32',
+                    content: '꾸안꾸',
+                    liked: false,
+                    likes: 8,
+                    user: '박도희',
+                    items: [{}, {}, {}, {}, {}]
+                },
+                {
+                    id: 4,
+                    img: 'https://t1.daumcdn.net/cfile/tistory/2263F34952AAF79910',
+                    content: '재미없는 남자 스타일',
+                    liked: true,
+                    likes: 12,
+                    user: '허성수',
+                    items: [{}, {}, {}, {}, {}]
+                },
+            ]
+        };
+        navigation.navigate('RecList', {rec: res.data});
+        // })
+    }
+
     return (
         <View>
             <View style={styles.centeredView}>
@@ -22,33 +127,38 @@ function CodiFormScreen({ navigation }) {
                 <View style={styles.centeredView}>
                 <View style={styles.modalView}>
                     <TouchableHighlight
-                    style={{ ...styles.openButton, backgroundColor: '#2196F3' }}
-                    onPress={() => {
-                        navigation.navigate('Camera', {category: uploadCategory});
-                        setModalVisible(!modalVisible);
-                    }}>
-                    <Text style={styles.textStyle}>카메라</Text>
+                        style={{ ...styles.openButton, backgroundColor: '#2196F3' }}
+                        onPress={() => {
+                            navigation.navigate('Camera', {category: uploadCategory});
+                            setModalVisible(!modalVisible);
+                        }}
+                    >
+                        <Text style={styles.textStyle}>카메라</Text>
                     </TouchableHighlight>
                     <TouchableHighlight
-                    style={{ ...styles.openButton, backgroundColor: '#2196F3' }}
-                    onPress={() => {
-                        setModalVisible(!modalVisible);
-                    }}>
-                    <Text style={styles.textStyle}>갤러리에서 가져오기</Text>
+                        style={{ ...styles.openButton, backgroundColor: '#2196F3' }}
+                        onPress={() => {
+                            pickImage();
+                            setModalVisible(!modalVisible);
+                        }}
+                    >
+                        <Text style={styles.textStyle}>갤러리에서 가져오기</Text>
                     </TouchableHighlight>
                     <TouchableHighlight
-                    style={{ ...styles.openButton, backgroundColor: '#2196F3' }}
-                    onPress={() => {
-                        setModalVisible(!modalVisible);
-                    }}>
-                    <Text style={styles.textStyle}>내 옷장에서 가져오기</Text>
+                        style={{ ...styles.openButton, backgroundColor: '#2196F3' }}
+                        onPress={() => {
+                            setModalVisible(!modalVisible);
+                        }}
+                    >
+                        <Text style={styles.textStyle}>내 옷장에서 가져오기</Text>
                     </TouchableHighlight>
                     <TouchableHighlight
-                    style={{ ...styles.openButton, backgroundColor: '#2196F3' }}
-                    onPress={() => {
-                        setModalVisible(!modalVisible);
-                    }}>
-                    <Text style={styles.textStyle}>닫기</Text>
+                        style={{ ...styles.openButton, backgroundColor: '#2196F3' }}
+                        onPress={() => {
+                            setModalVisible(!modalVisible);
+                        }}
+                    >
+                        <Text style={styles.textStyle}>닫기</Text>
                     </TouchableHighlight>
                 </View>
                 </View>
@@ -57,6 +167,7 @@ function CodiFormScreen({ navigation }) {
             <Text>
                 코디를 생성하는 폼 화면
             </Text>
+            {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
             <TouchableHighlight
                 style={styles.openButton}
                 onPress={() => {
@@ -121,6 +232,12 @@ function CodiFormScreen({ navigation }) {
                 }}>
                 <Text style={styles.textStyle}>악세서리</Text>
             </TouchableHighlight>
+            <TouchableHighlight
+                style={styles.recButton}
+                onPress={recommendationRequest}
+            >
+                <Text style={styles.textStyle}>추천해주세요</Text>
+            </TouchableHighlight>
         </View>
     )
 }
@@ -155,6 +272,14 @@ const styles = StyleSheet.create({
       elevation: 2,
       marginBottom: 10,
     },
+    recButton: {
+        height: 40,
+        backgroundColor: '#CD5C5C',
+        borderRadius: 20,
+        padding: 10,
+        elevation: 2,
+        marginBottom: 10,
+      },
     textStyle: {
       color: 'white',
       fontWeight: 'bold',
