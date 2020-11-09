0a3c8367436bf06b3c5a6c5327a312d222a5ecde
허성수 <gjtjdtn201@naver.com>
2020-11-10 01:04:01 +0900

    FIX | create coordi logic

--- backend/hss/wear/urls.py
+++ backend/hss/wear/urls.py
@@ -6,8 +6,8 @@ app_name = 'wear'
 
 urlpatterns = [
     # 코디 아이템 등록/삭제
-    path('userclothes/', views.userclothes.as_view()),
-    path('userclothes/<int:pk>', views.clothesdetail.as_view()),
+    path('userclothes/', views.UserClothesAPI.as_view()),
+    # path('userclothes/<int:pk>', views.clothesdetail.as_view()),
     # 내가 등록한 아이템 보기
     path('mylist/', views.mylist),
     # 코디셋 등록하기
@@ -15,13 +15,11 @@ urlpatterns = [
     # 내가 등록한 코디셋 보기
     path('listcoordi/', views.list_coordi),
     # 코디셋 좋아요/삭제 하기
-    path('likecoordi/coordi_pk/', views.like_coordi),
+    path('likecoordi/<int:coordi_pk>', views.like_coordi),
     # 내가 좋아요한 리스트 가져오기
     path('likelist/', views.like_list),
     # 코디 추천
     path('recommand/', views.recommand),
     path('coordiset/test/', views.coordiset_test),
-    # 이미지 전처리
-    path('image/preprocess/', views.image_preprocess),
     # path('guidetour/<int:tour_pk>', views.guidetour),
 ]
