62fcc363e204891e086ab9539905d661ebf91a76
박인영 <in0_2015@naver.com>
2020-11-11 18:41:30 +0900

    UPDATE | main design

---
+++ HeoSeongSoo/components/main/MainSelectText.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Button } from 'react-native-paper';
+
+function MainSelectText(props) {
+    return (
+        <Button
+        value={props.value}
+        style={{borderWidth: 1, borderColor: '#0d3754', width: 200, marginBottom: 15}}
+        labelStyle={{fontSize:20, fontWeight: 'bold', color: '#0d3754'}}
+        // onPress={() => console.log(props.value)}
+        onPress={props.onPress} 
+        >
+        {props.label}</Button>
+
+    )
+}
+
+export default MainSelectText;
