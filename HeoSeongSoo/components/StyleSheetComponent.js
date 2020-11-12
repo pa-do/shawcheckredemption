d4ccb3106773469533789c9d85e9d4982efde9ab
박도희 <dohee.pa@gmail.com>
2020-11-12 23:41:55 +0900

    UPDATE

--- HeoSeongSoo/components/StyleSheetComponent.js
+++ HeoSeongSoo/components/StyleSheetComponent.js
@@ -17,7 +17,7 @@ const styles = StyleSheet.create({
         backgroundColor: 'white',
         borderRadius: 20,
         padding: 35,
-        alignItems: 'stretch',
+        alignItems: 'center',
         shadowColor: '#000',
         shadowOffset: {
           width: 0,
