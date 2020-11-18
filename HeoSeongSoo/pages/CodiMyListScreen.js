6d6c6baacb249230d753c5e72b7ad135037dc8ec
박도희 <dohee.pa@gmail.com>
2020-11-18 10:36:11 +0900

    DELETE | unused code

--- HeoSeongSoo/pages/CodiMyListScreen.js
+++ HeoSeongSoo/pages/CodiMyListScreen.js
@@ -140,12 +140,10 @@ function CodiMyListScreen({ navigation, route }) {
     const [uploadedColor, setUploadedColor] = React.useState(null);
     const [uploadedItemPk, setUploadedItemPk] = React.useState(null);
     const [uploadCategory, setUploadCategory] = React.useState();
-    // 수정 중
     const [uploadedBrand, setUploadedBrand] = React.useState();
     const [uploadedItem, setUploadedItem] = React.useState();
     const [uploadedPrice, setUploadedPrice] = React.useState();
     const [uploadedUrl, setUploadedUrl] = React.useState();
-    // 수정 중
     const [codis, setCodis] = React.useState([]);
     const [likeCodis, setLikeCodis] = React.useState([]);
     const [userItems, setUserItems] = React.useState({});
@@ -258,18 +256,6 @@ function CodiMyListScreen({ navigation, route }) {
             }
         }
 
-        // if (isNaN(parseInt(uploadedPrice))) {
-        //     setUploadedPrice();
-        // } else {
-        //     setUploadedPrice(parseInt(uploadedPrice));
-        // }
-        // isNaN(parseInt(uploadedPrice))? undefined : parseInt(uploadedPrice)
-        
-        // brand: uploadedBrand? uploadedBrand: '',
-        // price: uploadedPrice? uploadedPrice: '',
-        // item: uploadedItem? uploadedItem: '',
-        // url: uploadedUrl? uploadedUrl: '',
-        
         const data = {
             R: uploadedColor[0],
             G: uploadedColor[1],
@@ -288,7 +274,6 @@ function CodiMyListScreen({ navigation, route }) {
             refreshItems();
             setUploadedBrand();
             setUploadedItem();
-            setUploadedPrice();
             setUploadedPrice();
             setUploadedUrl();
             setModalItemCategoryVisible(true);
