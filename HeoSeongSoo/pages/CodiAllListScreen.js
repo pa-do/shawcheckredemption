fdc058f51a984c28d3cd627fc17140af47b59710
조규성 <pandora_h@naver.com>
2020-11-19 16:48:32 +0900

    DELETE | log

--- HeoSeongSoo/pages/CodiAllListScreen.js
+++ HeoSeongSoo/pages/CodiAllListScreen.js
@@ -68,7 +68,6 @@ function CodiAllListScreen({ navigation, route }) {
             setIndex(index + 6);
             setRefreshing(false);
             setAllCodiList(refreshing ? allCodiList : allCodiList.concat(res.data));
-            console.log(res)
         })
         .catch(err => console.error(err))
     }
@@ -80,7 +79,6 @@ function CodiAllListScreen({ navigation, route }) {
             setIndex(6);
             setRefreshing(false);
             setAllCodiList(res.data);
-            console.log(res)
         })
     }
 
